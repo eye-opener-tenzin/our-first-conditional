65a8ec77e6cf24401338cd037735ae2b94e3c51d
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-22 21:36:31 -0700

    add html and js file

---
+++ firstCondition.js
@@ -0,0 +1,10 @@
+function isEven(num){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if(num % 2 == 0) {
+       console.log("even")
+       document.write('even')
+   }
+   
+}
+isEven(4);
+isEven(10);
